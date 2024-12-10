c851b4b5f772611a8ac035a15ceffd5b8762b4a0
Sergey <seleznev-krista@mail.ru>
2024-12-10 07:01:16 +0300

    feat: add ErrorBoundary

---
+++ rs-react-app/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,27 @@
+import { Component, ErrorInfo } from 'react';
+
+interface Props {
+  children: JSX.Element;
+}
+
+export class ErrorBoundary extends Component<Props> {
+  state = {
+    error: null,
+    errorInfo: null,
+  };
+
+  componentDidCatch = (error: Error, errorInfo: ErrorInfo) => {
+    this.setState({
+      error: error,
+      errorInfo: errorInfo,
+    });
+    console.log('errorInfo', errorInfo);
+  };
+
+  render() {
+    if (this.state.errorInfo) {
+      return <h1>Что-то пошло не так</h1>;
+    }
+    return this.props.children;
+  }
+}
--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary.tsx';
 
 const root = document.createElement('div');
+document.body.appendChild(root);
 
-createRoot(root).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
 );
