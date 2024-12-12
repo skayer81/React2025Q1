024c44112d9f8f284aadba6009014ffcb989a4d3
Sergey <seleznev-krista@mail.ru>
2024-12-12 10:26:14 +0300

    refactor: del comments

--- rs-react-app/src/components/loadingOverlay/loadingOverlay.tsx
+++ rs-react-app/src/components/loadingOverlay/loadingOverlay.tsx
@@ -2,32 +2,17 @@ import { Component } from 'react';
 import './loadingOverlay.css';
 
 interface Props {
-  //onChange: (inputValue: string) => void;
   isLoading: boolean;
   message: string;
 }
 
 export class LoadingOverlay extends Component<Props> {
-  // placeholderText = 'Enter a search term';
-
-  //   onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //     if (!event.target) return;
-  //     this.props.onChange(event.target.value);
-  //   };
-
   render() {
     return (
       <>
         {this.props.isLoading && (
           <div className="overlay">
             <p>{this.props.message}</p>
-            {/* <input
-              type="text"
-              onChange={this.onChange}
-              defaultValue={this.props.inputValue}
-              className="form-input"
-              placeholder={this.placeholderText}
-            ></input> */}
           </div>
         )}
       </>
