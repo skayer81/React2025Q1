669940d89591b72b5ab9751f404ed3d4f5965de1
Sergey <seleznev-krista@mail.ru>
2025-02-01 19:09:08 +0300

    refactor: change SearchForm to function component

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, Component, FormEvent } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { ErrorButton } from '../ErrorButton/ErrorButton';
 import { LocaleStorageAPI } from '../API/LocaleStorageAPI';
 import style from './searchForm.module.css';
@@ -7,46 +7,56 @@ interface Props {
   onClick: (value: string) => void;
 }
 
-export class SearchForm extends Component<Props> {
-  localeStorageAPI = new LocaleStorageAPI();
+const localeStorageAPI = new LocaleStorageAPI();
 
-  state = {
-    inputValue: '',
+export function SearchForm(props: Props) {
+  const [inputValue, setInputValue] = useState('');
+  // state = {
+  //   inputValue: '',
+  // };
+
+  // componentDidMount() {
+  //   if (!this.localeStorageAPI.hasSave()) {
+  //     return;
+  //   }
+
+  //   const request = this.localeStorageAPI.loadRequest();
+  //   this.setState({ inputValue: request });
+  //   this.props.onClick(request);
+  // }
+
+  useEffect(() => {
+    if (!localeStorageAPI.hasSave()) return;
+    const request = localeStorageAPI.loadRequest();
+    setInputValue(request);
+    props.onClick(request);
+  }, [props]);
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    //this.setState({ inputValue: event.target.value });
+    setInputValue(event.target.value);
   };
 
-  componentDidMount() {
-    if (!this.localeStorageAPI.hasSave()) {
-      return;
-    }
-
-    const request = this.localeStorageAPI.loadRequest();
-    this.setState({ inputValue: request });
-    this.props.onClick(request);
-  }
-
-  onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    this.setState({ inputValue: event.target.value });
-  };
-
-  onClick = (event: FormEvent<HTMLFormElement>) => {
+  const onClick = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    this.localeStorageAPI.saveRequest(this.state.inputValue);
-    this.props.onClick(this.state.inputValue);
+    localeStorageAPI.saveRequest(inputValue);
+    props.onClick(inputValue);
+    //  this.props.onClick(this.state.inputValue);
   };
 
-  render() {
-    return (
-      <form className={style.form} onSubmit={this.onClick}>
-        <span className={style.formTitle}>search form: </span>
-        <input
-          value={this.state.inputValue}
-          onChange={this.onChange}
-          placeholder="Enter a search term"
-          className={style.formInput}
-        />
-        <button type="submit">{'Search'}</button>
-        <ErrorButton />
-      </form>
-    );
-  }
+  // render() {
+  return (
+    <form className={style.form} onSubmit={onClick}>
+      <span className={style.formTitle}>search form: </span>
+      <input
+        value={inputValue}
+        onChange={onChange}
+        placeholder="Enter a search term"
+        className={style.formInput}
+      />
+      <button type="submit">{'Search'}</button>
+      <ErrorButton />
+    </form>
+  );
+  // }
 }
