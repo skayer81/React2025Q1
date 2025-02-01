07341acb232ce7c9360db0a2acaac69deb4b669a
Sergey <seleznev-krista@mail.ru>
2025-02-01 13:12:58 +0300

    refactor: change SearchResult to functional component

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -1,4 +1,4 @@
-import { PureComponent } from 'react';
+import { useEffect, useState } from 'react';
 import { Card } from './Card/Card';
 import { getAnimal, getAnimals } from '../API/StAPI';
 import { Animal } from '../interfaces/interfaces';
@@ -9,74 +9,108 @@ interface Props {
   request?: string;
 }
 
-interface State {
-  searchResult: Array<Animal> | null;
-  isLoading: boolean;
-  error: string | null;
-}
+// interface State {
+//   searchResult: Array<Animal> | null;
+//   isLoading: boolean;
+//   error: string | null;
+// }
 
-export class SearchResult extends PureComponent<Props, State> {
-  state: State = {
-    searchResult: null,
-    isLoading: false,
-    error: null,
-  };
+export function SearchResult(props: Props) {
+  const [searchResult, setSearchResult] = useState<Array<Animal> | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
-  componentDidMount() {
-    this.load();
-  }
+  const { request } = props;
+  // state: State = {
+  //   searchResult: null,
+  //   isLoading: false,
+  //   error: null,
+  // };
 
-  componentDidUpdate(prevProps: Props) {
-    if (this.props.request !== prevProps.request) {
-      this.load();
-    }
-  }
+  // const load = async () => {
+  //   //this.setState({ isLoading: true, error: null });
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const res = request ? await getAnimal(request) : await getAnimals();
 
-  load = async () => {
-    this.setState({ isLoading: true, error: null });
-    try {
-      const res = this.props.request
-        ? await getAnimal(this.props.request)
-        : await getAnimals();
+  //     // this.setState({ searchResult: res.animals });
+  //     setSearchResult(res.animals);
+  //   } catch (err) {
+  //     // this.setState({ error: 'Error loading data' });
+  //     setError('Error loading data');
+  //     console.log(err);
+  //   } finally {
+  //     setIsLoading(false);
+  //     //  this.setState({ isLoading: false });
+  //   }
+  // };
 
-      this.setState({ searchResult: res.animals });
-    } catch (err) {
-      this.setState({ error: 'Error loading data' });
-      console.log(err);
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  };
+  // componentDidMount() {
+  //   this.load();
+  // }
 
-  render() {
-    const { request } = this.props;
-    const { searchResult, isLoading, error } = this.state;
+  // useEffect(() => {
+  //   load()
+  // });
 
-    return (
-      <>
-        <section className={style.searchResult}>
-          <h2>{request ? 'Search result' : 'Full catalog'}</h2>
+  // componentDidUpdate(prevProps: Props) {
+  //   if (this.props.request !== prevProps.request) {
+  //     this.load();
+  //   }
+  // }
+  useEffect(() => {
+    // if (this.props.request !== prevProps.request) {
+    // load();
+    //    }
+    const load = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const res = request ? await getAnimal(request) : await getAnimals();
 
-          {error && <p>{error}</p>}
-          {!isLoading && searchResult && searchResult.length === 0 && (
-            <p>nothing found</p>
-          )}
-          {searchResult && searchResult.length > 0 && (
-            <ul className={style.listItems}>
-              {searchResult.map((animal, index) => (
-                <li key={animal.uid}>
-                  <Card
-                    index={index + 1}
-                    name={animal.name}
-                    earthAnimal={animal.earthAnimal}
-                  />
-                </li>
-              ))}
-            </ul>
-          )}
-        </section>
-        <LoadingOverlay isLoading={this.state.isLoading} message="Loading..." />
-      </>
-    );
-  }
+        // this.setState({ searchResult: res.animals });
+        setSearchResult(res.animals);
+      } catch (err) {
+        // this.setState({ error: 'Error loading data' });
+        setError('Error loading data');
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+        //  this.setState({ isLoading: false });
+      }
+    };
+    load();
+  }, [request]);
+
+  // render() {
+  //const { searchResult, isLoading, error } = this.state;
+
+  return (
+    <>
+      <section className={style.searchResult}>
+        <h2>{request ? 'Search result' : 'Full catalog'}</h2>
+
+        {error && <p>{error}</p>}
+        {!isLoading && searchResult && searchResult.length === 0 && (
+          <p>nothing found</p>
+        )}
+        {searchResult && searchResult.length > 0 && (
+          <ul className={style.listItems}>
+            {searchResult.map((animal, index) => (
+              <li key={animal.uid}>
+                <Card
+                  index={index + 1}
+                  name={animal.name}
+                  earthAnimal={animal.earthAnimal}
+                />
+              </li>
+            ))}
+          </ul>
+        )}
+      </section>
+      <LoadingOverlay isLoading={isLoading} message="Loading..." />
+    </>
+  );
+  // }
 }
