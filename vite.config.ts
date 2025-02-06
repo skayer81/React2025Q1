94f4e6165b0e27a90465dbf8991f99bcb3a6ba92
Sergey <seleznev-krista@mail.ru>
2025-02-06 08:12:51 +0300

    feat: add Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,36 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    coverage: {
+      all: true,
+      exclude: [
+        'src/test/**/*',
+        '**/types.ts',
+        '**/types/*',
+        '**/interfaces.ts',
+        '**/*.d.ts',
+        '**/index.ts',
+        'src/main.tsx',
+        'src/config/**/*',
+        'src/**/*/enums.ts',
+        'src/setupTest.ts',
+        // 'src/**/*/*.test.tsx',
+        '**/*.test.tsx',
+      ],
+      extension: ['.ts', '.tsx'],
+      include: ['src/**/*'],
+      provider: 'v8',
+      reporter: ['text'],
+    },
+    css: false,
+    environment: 'jsdom',
+    globals: true,
+    maxConcurrency: 8,
+    setupFiles: ['src/setupTest.ts'],
+  },
 });
